34fdbe86619ce69ff68b5d404e56f8785fa02dda
LYNX-kvm <kato.vanmulders07@gmail.com>
2024-12-22 15:27:19 +0100

    added function
    checks if lowest input is smaller than highest input

--- random_number_generator.py
+++ random_number_generator.py
@@ -4,8 +4,11 @@ value_low = input("Lowest value?: ")
 value_high = input("Highest value?: ")
 
 if value_low.isdigit() and value_high.isdigit():
-  value_low = int(value_low)
-  value_high = int(value_high)
-  print(randint(value_low, value_high))
+    value_low = int(value_low)
+    value_high = int(value_high)
+    if value_low <= value_high:
+        print(randint(value_low, value_high))
+    else:
+        print("Error, lowest value is greater than highest value")
 else:
-  print("Error, imputs are not numbers")
+    print("Error, inputs are not numbers")
