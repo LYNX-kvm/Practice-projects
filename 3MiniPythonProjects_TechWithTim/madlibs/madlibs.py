c2410c1a0bf21e7c96b3b50977b85920bb04fbbb
LYNX-kvm <kato.vanmulders07@gmail.com>
2024-12-22 15:59:07 +0100

    fucking hell

--- 3MiniPythonProjects_TechWithTim/madlibs/madlibs.py
+++ 3MiniPythonProjects_TechWithTim/madlibs/madlibs.py
@@ -2,7 +2,7 @@
 # put story in story.txt
 
 with open("story.txt", "r") as f:
-  story = f.read()
+    story = f.read()
 
 words = set()
 start_of_word = -1
@@ -11,20 +11,21 @@ target_start = "<"
 target_end = ">"
 
 for i, char in enumerate(story):
-  if char == target_start:
-    start_of_word = i
-  if char == target_end and start_of_word != -1:
-    word = story[start_of_word: i + 1]
-    words.add(word) 
-    start_of_word = -1
+    if char == target_start:
+        start_of_word = i
+
+    if char == target_end and start_of_word != -1:
+        word = story[start_of_word: i + 1]
+        words.add(word)
+        start_of_word = -1
 
 answers = {}
 
-for word in words: 
-  answer = input("Enter a word for " + word + ": ")
-  answers[word] = answer
+for word in words:
+    answer = input("Enter a word for " + word + ": ")
+    answers[word] = answer
 
 for word in words:
-  story = story.replace(word, answers[word])
+    story = story.replace(word, answers[word])
 
 print(story)
